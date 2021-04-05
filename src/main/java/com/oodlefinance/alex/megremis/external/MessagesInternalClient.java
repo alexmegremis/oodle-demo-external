57830863e5d89306e3e0186f5c2814c4faf21105
Alex Megremis <>
2021-04-05 01:11:22 +0100

    Patching the client to a normal controller. Maybe the oddity can be discussed during the interview.

--- src/main/java/com/oodlefinance/alex/megremis/external/MessagesInternalClient.java
+++ src/main/java/com/oodlefinance/alex/megremis/external/MessagesInternalClient.java
@@ -2,22 +2,24 @@ package com.oodlefinance.alex.megremis.external;
 
 import com.oodlefinance.alex.megremis.external.api.v1.MessageDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(value = "Internal-API")
-@RequestMapping("/api/v1/message")
+@FeignClient(value = "Internal-API", decode404 = true, path = "/api/v1/message")
+//@RequestMapping("/api/v1/message")
+//@RequestMapping("/")
 public interface MessagesInternalClient {
     @GetMapping()
     List<MessageDTO> getAll();
 
     @GetMapping("{id}")
-    MessageDTO get(@PathVariable("id") final Integer id);
+    ResponseEntity<MessageDTO> get(@PathVariable("id") final Integer id);
 
     @PostMapping()
     Integer save(@RequestBody final String message);
 
     @DeleteMapping ("{id}")
-    void delete(@PathVariable (name = "id") final Integer id);
+    ResponseEntity delete(@PathVariable (name = "id") final Integer id);
 }
--- src/main/java/com/oodlefinance/alex/megremis/external/api/v1/MessageController.java
+++ src/main/java/com/oodlefinance/alex/megremis/external/api/v1/MessageController.java
@@ -2,12 +2,13 @@ package com.oodlefinance.alex.megremis.external.api.v1;
 
 import com.oodlefinance.alex.megremis.external.MessagesInternalClient;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping ("/api/v2/message")
+@RequestMapping ("/api/v1/message")
 @Slf4j
 public class MessageController {
 
@@ -27,19 +28,19 @@ public class MessageController {
     public List<MessageDTO> getAllMessages() {
         return client.getAll();
     }
-//
-//    @GetMapping (value = "{id}")
-//    @ResponseBody
-//    public ResponseEntity getMessage(@PathVariable (name = "id") final Integer id) {
-//        ResponseEntity result = client.get(id);
-//
-//        return result;
-//    }
-//
-//    @DeleteMapping (value = "{id}")
-//    public ResponseEntity delete(@PathVariable (name = "id") final Integer id) {
-//        ResponseEntity result = client.delete(id);
-//
-//        return result;
-//    }
+
+    @GetMapping (value = "{id}")
+    @ResponseBody
+    public ResponseEntity<MessageDTO> getMessage(@PathVariable (name = "id") final Integer id) {
+        ResponseEntity result = client.get(id);
+
+        return result;
+    }
+
+    @DeleteMapping (value = "{id}")
+    public ResponseEntity delete(@PathVariable (name = "id") final Integer id) {
+        ResponseEntity result = client.delete(id);
+
+        return result;
+    }
 }
